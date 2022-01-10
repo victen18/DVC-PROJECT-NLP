2eab705abe3c14af81317cdebbc4c8d44c1da152
victen <vikramnimmakuri@gmail.com>
2022-01-10 06:02:28 -0800

    stage_02 update and data managment file created.

--- src/stage_02_prepare.py
+++ src/stage_02_prepare.py
@@ -4,10 +4,11 @@ import shutil
 from tqdm import tqdm
 import logging
 from src.utils.common import read_yaml, create_directories
+from src.utils.data_mgmt import process_posts
 import random
 
 
-STAGE = "STAGE_NAME" ## <<< change stage name 
+STAGE = "stage_02_prepare" ## <<< change stage name 
 
 logging.basicConfig(
     filename=os.path.join("logs", 'running_logs.log'), 
@@ -18,10 +19,33 @@ logging.basicConfig(
 
 
 def main(config_path, params_path):
-    ## read config files
+    
     config = read_yaml(config_path)
     params = read_yaml(params_path)
-    pass
+    
+    local_data_dir = config["source_data_dir"]["data_dir"]
+    local_data_file = config["source_data_dir"]["data_file"]
+    create_directories([local_data_dir])
+    input_data = os.path.join(local_data_dir,local_data_file)
+
+    split = params["prepared"]["split"]
+    seed = params["prepared"]["seed"]
+
+    random.seed(seed)
+
+    artifacts = config["artifacts"]
+    prepared_data_dir_path = os.path.join(artifacts["ARTIFACTS_DIR"],artifacts["PREPARED_DATA"])
+
+    create_directories([prepared_data_dir_path])
+
+    train_data_path = os.path.join(prepared_data_dir_path,artifacts["TRAIN_DATA"])
+    test_data_path = os.path.join(prepared_data_dir_path,artifacts["TEST_DATA"])
+
+    encode = "utf8"
+    with open(input_data,encoding="utf8") as f_in:
+        with open(train_data_path,"w",encoding=encode) as f_out_train:
+            with open(test_data_path,"w",encoding=encode) as f_out_test:
+                process_posts(f_in,f_out_train,f_out_test,"<python>",split)
 
 
 if __name__ == '__main__':
