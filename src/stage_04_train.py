7375928a03fd23e3f8e7f53bb335541e81c89830
victen <vikramnimmakuri@gmail.com>
2022-01-11 05:26:47 -0800

    stage_04 train completed

--- src/stage_04_train.py
+++ src/stage_04_train.py
@@ -1,30 +1,69 @@
 import argparse
 import os
-import shutil
+import joblib
 from tqdm import tqdm
 import logging
 from src.utils.common import read_yaml, create_directories
-import random
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 
 
-STAGE = "STAGE_NAME" ## <<< change stage name 
+STAGE = "stage_04_train"  ## <<< change stage name
 
 logging.basicConfig(
-    filename=os.path.join("logs", 'running_logs.log'), 
-    level=logging.INFO, 
+    filename=os.path.join("logs", "running_logs.log"),
+    level=logging.INFO,
     format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
-    filemode="a"
-    )
+    filemode="a",
+)
 
 
-def main(config_path, params_path):
-    ## read config files
+def train(config_path, params_path):
+
     config = read_yaml(config_path)
     params = read_yaml(params_path)
-    pass
+
+    artifacts = config["artifacts"]
+    featurized_data_dir_path = os.path.join(
+        artifacts["ARTIFACTS_DIR"], artifacts["FEATURIZED_DATA"]
+    )
+    featurized_train_data_path = os.path.join(
+        featurized_data_dir_path, artifacts["FEATURIZED_TRAIN_DATA"]
+    )
+
+    model_dir_path = os.path.join(artifacts["ARTIFACTS_DIR"], artifacts["MODEL_DIR"])
+    create_directories([model_dir_path])
+
+    model_path = os.path.join(model_dir_path, artifacts["MODEL_NAME"])
+
+    matrix = joblib.load(featurized_train_data_path)
+    labels = np.squeeze(matrix[:, 1].toarray())
+    x = matrix[:, 2:]
+
+    logging.info(f"input matrix size:{matrix.shape}")
+    logging.info(f"x matrix size:{x.shape}")
+    logging.info(f"y matrix size or label size: {labels.shape}")
+
+    seed = params["train"]["seed"]
+    n_est = params["train"]["n_est"]
+    min_split = params["train"]["min_split"]
+    n_jobs = params["train"]["n_jobs"]
+
+    model = RandomForestClassifier(
+        n_estimators=n_est,
+        min_samples_split=min_split,
+        n_jobs=n_jobs,
+        random_state=seed,
+    )
+
+    model.fit(x, labels)
+
+    joblib.dump(model, model_path)
+
+    logging.info(f"Model is trained and saved at {model_path}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", "-c", default="configs/config.yaml")
     args.add_argument("--params", "-p", default="params.yaml")
@@ -33,7 +72,7 @@ if __name__ == '__main__':
     try:
         logging.info("\n********************")
         logging.info(f">>>>> stage {STAGE} started <<<<<")
-        main(config_path=parsed_args.config, params_path=parsed_args.params)
+        train(config_path=parsed_args.config, params_path=parsed_args.params)
         logging.info(f">>>>> stage {STAGE} completed!<<<<<\n")
     except Exception as e:
         logging.exception(e)
